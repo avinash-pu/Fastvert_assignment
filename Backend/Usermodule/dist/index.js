8c7cb193c696fe6babdf21e99716cfd9337c503b
Avinash <avinashkumarsinha01@gmail.com>
2024-07-31 00:07:26 +0530

    updated babel

--- Backend/Usermodule/dist/index.js
+++ Backend/Usermodule/dist/index.js
@@ -1,17 +1,14 @@
-'use strict';
+"use strict";
 
 var express = require('express');
 var cors = require('cors');
-
 var connectDB = require('./databse/db');
 //const userRoutes = require('./routes/userrout');
 var employeeRoutes = require('./routes/employeeRoutes'); //change
 require('dotenv').config();
-
 var app = express();
 app.use(cors());
 app.use(express.json());
-
 connectDB();
 
 //app.use('/api',userRoutes);
@@ -19,5 +16,5 @@ app.use('/api', employeeRoutes); //changes
 
 var PORT = process.env.PORT || 5001;
 app.listen(PORT, function () {
-  return console.log('server running on port ' + PORT);
+  return console.log("server running on port ".concat(PORT));
 });
--- Backend/Usermodule/dist/modal/h1.js
+++ Backend/Usermodule/dist/modal/h1.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 var mongoose = require('mongoose');
-
 var h1Schema = new mongoose.Schema({
-  header: { type: String }
+  header: {
+    type: String
+  }
 });
-
 module.exports = mongoose.model('H1', h1Schema);
