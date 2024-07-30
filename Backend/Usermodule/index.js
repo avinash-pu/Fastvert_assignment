4b163131d2e13402ebe5a03dfd0b92afd26805e4
Avinash <avinashkumarsinha01@gmail.com>
2024-07-31 01:52:10 +0530

    test

--- Backend/Usermodule/index.js
+++ Backend/Usermodule/index.js
@@ -4,12 +4,18 @@ const cors = require('cors');
 
 
 const connectDB = require('./src/databse/db');
+// Custom CORS configuration
+const corsOptions = {
+    origin: '*',  // Replace with your frontend URL or an array of URLs
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  };
 //const userRoutes = require('./routes/userrout');
 const employeeRoutes = require('./src/routes/employeeRoutes');  //change
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 connectDB();
