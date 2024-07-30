0091ca81982b9241c78e5bf0ecffc2af50eaf9d8
Avinash <avinashkumarsinha01@gmail.com>
2024-07-30 23:58:11 +0530

    updatedb

--- Backend/Usermodule/dist/midlleware/auth.js
+++ Backend/Usermodule/dist/midlleware/auth.js
@@ -1,21 +1,19 @@
-"use strict";
+'use strict';
 
 var jwt = require('jsonwebtoken');
+
 var auth = function auth(req, res, next) {
-  var token = req.header('x-auth-token');
-  if (!token) {
-    return res.status(401).json({
-      msg: 'no token, authorization denied'
-    });
-  }
-  try {
-    var decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    res.status(401).json({
-      msg: 'Token is not valid'
-    });
-  }
+    var token = req.header('x-auth-token');
+    if (!token) {
+        return res.status(401).json({ msg: 'no token, authorization denied' });
+    }
+
+    try {
+        var decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
 };
 module.exports = auth;
--- Backend/Usermodule/dist/modal/history.js
+++ Backend/Usermodule/dist/modal/history.js
@@ -1,24 +1,12 @@
-"use strict";
+'use strict';
 
 var mongoose = require('mongoose');
+
 var historySchema = new mongoose.Schema({
-  employeeId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Employee',
-    required: true
-  },
-  changeDate: {
-    type: Date,
-    "default": Date.now
-  },
-  changeType: {
-    type: String,
-    "enum": ['CREATE', 'UPDATE', 'DELETE'],
-    required: true
-  },
-  changedData: {
-    type: mongoose.Schema.Types.Mixed,
-    required: true
-  }
+    employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
+    changeDate: { type: Date, 'default': Date.now },
+    changeType: { type: String, 'enum': ['CREATE', 'UPDATE', 'DELETE'], required: true },
+    changedData: { type: mongoose.Schema.Types.Mixed, required: true }
 });
+
 module.exports = mongoose.model('History', historySchema);
